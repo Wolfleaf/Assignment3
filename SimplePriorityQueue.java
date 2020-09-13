9f4b62e4fdfd01ad3542adb7a428f3c67e4494cc
Wolfleaf <bluepandaleaf@gmail.com>
2020-09-13 15:01:56 -0600

    Add files via upload

--- SimplePriorityQueue.java
+++ SimplePriorityQueue.java
@@ -4,42 +4,59 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 
+/**
+ * A Generic List of items that are sorted using Comparable from largest to smallest
+ * 
+ * @author Alex Thomsen and Guyan Cool
+ * Current Version: 9/13/2020
+ *
+ * @param <Type>	Any object that implements Comparable and can be sorted
+ */
 public class SimplePriorityQueue<Type> implements PriorityQueue<Type>, Comparable<Type> {
 
 	private Type[] data; // array where the data is stored
 
-	private int actualLength; // length of the actual array
+	private int actualLength; // length of the array
 
 	private int dynamicLength; // length of the parts of the array we are actually using
 
+	/**
+	 * Constructor for SimplePriorityQueue
+	 * Starts with an array of size 16
+	 */
 	@SuppressWarnings("unchecked")
 	public SimplePriorityQueue() {
-//		make array
-//		finding min and removing min should run fastest
-//		minimum item will always be the item with the largest index
-//		begin as empty array
-//		natural ordering
-
 		this.data = (Type[]) new Object[16];
 		this.actualLength = 16;
 		this.dynamicLength = 0;
 	}
 
-	// name to comparator parameter added
+
+	/**
+	 * Constructor for SimplePriorityQueue that utilizes Comparator
+	 * Starts with an array of size 16
+	 * 
+	 * @param Comp type that can be used in Comparator
+	 */
 	@SuppressWarnings("unchecked")
 	public SimplePriorityQueue(Comparator<? super Type> Comp) {
-
 		this.data = (Type[]) new Object[16];
 		this.actualLength = 16;
 		this.dynamicLength = 0;
 	}
 
+	/**
+	 * Returns the smallest variable in the priority queue
+	 */
 	@Override
 	public Type findMin() throws NoSuchElementException {
 
 		return this.data[dynamicLength - 1];
 	}
 
+	/**
+	 * Deletes the smallest variable in the priority queue
+	 */
 	@Override
 	public Type deleteMin() throws NoSuchElementException {
 
@@ -49,93 +66,115 @@ public class SimplePriorityQueue<Type> implements PriorityQueue<Type>, Comparabl
 		return deletedMin;
 	}
 
+	/**
+	 * Inserts an item of the Priority Queue type into the priority queue
+	 * This class automatically sorts the queue and places the item in the correct location
+	 * 
+	 * @param item 	Item being inserted into the priority queue
+	 */
 	@SuppressWarnings("unchecked")
 	public void insert(Type item) {
-
+		
 		int min = 0;
 		int max = dynamicLength;
 		int mid = 0;
 		boolean itemFound = false;
-
+		
 		this.dynamicLength++;
-
+		
 		while (!itemFound) {
 			mid = (((max - min) / 2) + min);
 
-			if (this.data[mid] == item || max <= min) { // If item equals data
+			if (this.data[mid] == item || max <= min) { //If item equals data
 				itemFound = true;
-			} else if (((Comparable<Type>) item).compareTo(this.data[mid]) < 0) { // If item is smaller than data
+			}
+			else if (((Comparable<Type>) item).compareTo(this.data[mid]) < 0) { //If item is smaller than data
 				min = mid + 1;
-			} else if (((Comparable<Type>) item).compareTo(this.data[mid]) > 0) { // If item is larger than data
+			}
+			else if (((Comparable<Type>) item).compareTo(this.data[mid]) > 0) { //If item is larger than data
 				max = mid;
 			}
 		}
-
+		
 		Type[] newData = (Type[]) new Object[actualLength];
 
-		for (int indexFirst = 0; indexFirst < mid; indexFirst++) {
-			newData[indexFirst] = this.data[indexFirst];
-		}
-		newData[mid] = item;
-		for (int indexSecond = mid; indexSecond < dynamicLength - 1; indexSecond++) {
-			newData[indexSecond + 1] = this.data[indexSecond];
-		}
-
+        for (int indexFirst = 0; indexFirst < mid; indexFirst++) {
+            newData[indexFirst] = this.data[indexFirst];
+        }
+        newData[mid] = item;
+        for (int indexSecond = mid; indexSecond < dynamicLength - 1; indexSecond++) {
+            newData[indexSecond + 1] = this.data[indexSecond];
+        }
+        
 		this.data = newData;
 	}
 
+	/**
+	 * Inserts multiple items stored in a collection type into the priority queue
+	 * Automatically sorts the items into their correct location
+	 * 
+	 * @param coll	Collection of items to be inserted
+	 */
 	@Override
 	public void insertAll(Collection<? extends Type> coll) {
-			
 		for (Type var : coll) {
 			this.insert(var);
 		}
+
 	}
 
+	/**
+	 * Outputs the amount of items in the priority queue
+	 */
 	@Override
 	public int size() {
-
 		return dynamicLength;
 	}
 
+	/**
+	 * Boolean expression that returns true if there are no items in the priority queue
+	 */
 	@Override
 	public boolean isEmpty() {
-	
-		if (this.data[0] == null) {
+		if (this.data[0] == null)
+		{
 			return true;
 		}
 		return false;
 	}
 
+	/**
+	 * Removes all items from the priority queue
+	 */
 	@Override
 	public void clear() {
-		
-		Type[] newData = (Type[]) new Object[actualLength];
-		this.data = newData;
-		this.dynamicLength = 0;
-
+		for (Type var : data) {
+			var = null;
+		}
+		dynamicLength = 0;
 	}
 
+	/**
+	 * Compares two items for sorting in the priority queue
+	 */
 	@Override
 	public int compareTo(Type item) {
-
 		return this.compareTo(item);
 	}
-
+	
 	/**
 	 * Returns the data of the SimplePriorityQueue in an easy to read format
 	 * Example: {5, 4, 3, 2, 1}
 	 */
 	public String toString() {
-        String stringQueue = "{";
-        for (int i = 0; i < dynamicLength; i++) {
-            stringQueue += data[i];
-            if (i != dynamicLength - 1){
-                stringQueue += ", ";
-            } 
-        }
-        stringQueue += "}";
-        return stringQueue;
-    }
-
+		String stringQueue = "{";
+		for (int i = 0; i < dynamicLength; i++) {
+			stringQueue += data[i];
+			if (i != dynamicLength - 1){
+				stringQueue += ", ";
+			} 
+		}
+		stringQueue += "}";
+		return stringQueue;
+	}
 }
--- SimplePriorityQueueTest.java
+++ SimplePriorityQueueTest.java
@@ -2,41 +2,233 @@ package assign03;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimplePriorityQueueTest {
+	
+	private SimplePriorityQueue<Integer> queueInt;  // library that uses names to identify patrons (holders)
+	private SimplePriorityQueue<Integer> queueInt2;  // library that uses names to identify patrons (holders)
+	private SimplePriorityQueue<String> queueString;  // library that uses names to identify patrons (holders)
+	private SimplePriorityQueue<String> queueString2;  // library that uses names to identify patrons (holders)
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		queueInt = new SimplePriorityQueue<Integer>();
+		queueInt.insert(1);
+		queueInt.insert(2);
+		queueInt.insert(3);
+		queueInt.insert(4);
+		queueInt.insert(5);
+		
+		queueInt2 = new SimplePriorityQueue<Integer>();
+		queueInt2.insert(-40);
+		queueInt2.insert(-20);
+		queueInt2.insert(0);
+		queueInt2.insert(20);
+		queueInt2.insert(40);
 
+		queueString = new SimplePriorityQueue<String>();
+		queueString.insert("A");
+		queueString.insert("B");
+		queueString.insert("C");
+		queueString.insert("D");
+		queueString.insert("E");
+		
+		queueString2 = new SimplePriorityQueue<String>();
+		queueString2.insert("A");
+		queueString2.insert("E");
+		queueString2.insert("F");
+		queueString2.insert("S");
+		queueString2.insert("X");
+	}
+	
+	@Test
+	void testFindMinIntArray() {
+		assertEquals(1, queueInt.findMin());
+		assertEquals(-40, queueInt2.findMin());
+	}
+	
+	@Test
+	void testFindMinStringArray() {
+		assertEquals("A", queueString.findMin());
+		assertEquals("A", queueString2.findMin());
+	}
+	
+	@Test
+	void testDelMinIntArray() {
+		queueInt.deleteMin();
+		queueInt2.deleteMin();
+		assertEquals("{5, 4, 3, 2}", queueInt.toString());
+		assertEquals("{40, 20, 0, -20}", queueInt2.toString());
+		
+		queueInt.deleteMin();
+		queueInt2.deleteMin();
+		assertEquals("{5, 4, 3}", queueInt.toString());
+		assertEquals("{40, 20, 0}", queueInt2.toString());
+	}
+	
+	@Test
+	void testDelMinStringArray() {
+		queueString.deleteMin();
+		queueString2.deleteMin();
+		assertEquals("{E, D, C, B}", queueString.toString());
+		assertEquals("{X, S, F, E}", queueString2.toString());
+		
+		queueString.deleteMin();
+		queueString2.deleteMin();
+		assertEquals("{E, D, C}", queueString.toString());
+		assertEquals("{X, S, F}", queueString2.toString());
+	}
+	
 	@Test
 	void testIntegerInsert() {
-
-		SimplePriorityQueue<Integer> testQueue = new SimplePriorityQueue<Integer>();
-
-		testQueue.insert(1);
-		testQueue.insert(2);
-		testQueue.insert(3);
-		testQueue.insert(100);
-		testQueue.insert(4);
-		testQueue.insert(-100);
-
-		assertEquals("{100, 4, 3, 2, 1, -100}", testQueue.toString());
+		queueInt.insert(100);
+		queueInt.insert(-100);
+		queueInt.insert(1);
+		assertEquals("{100, 5, 4, 3, 2, 1, 1, -100}", queueInt.toString());
 	}
 	
 	@Test
 	void testStringInsert() {
+		queueString.insert("A");
+		queueString.insert("C");
+		queueString.insert("Z");
 
-		SimplePriorityQueue<String> testQueue = new SimplePriorityQueue<String>();
+		assertEquals("{Z, E, D, C, C, B, A, A}", queueString.toString());
+	}
+	
+	@Test
+	void testIntegerInsertAll() {
+		ArrayList<Integer> inserted = new ArrayList<Integer>();
+		inserted.add(4);
+		inserted.add(5);
+		inserted.add(6);
+		inserted.add(7);
+		inserted.add(0);
+		
+		queueInt.insertAll(inserted);
+		assertEquals("{7, 6, 5, 5, 4, 4, 3, 2, 1, 0}", queueInt.toString());
+	}
+	
+	@Test
+	void testStringInsertAll() {
+		ArrayList<String> inserted = new ArrayList<String>();
+		inserted.add("A");
+		inserted.add("W");
+		inserted.add("N");
+		inserted.add("L");
+		inserted.add("Y");
+		
+		queueString.insertAll(inserted);
+		assertEquals("{Y, W, N, L, E, D, C, B, A, A}", queueString.toString());
+	}
+	
+	@Test
+	void testIntegerSize() {
+		assertEquals(5, queueInt.size());
+		assertEquals(5, queueInt2.size());
+	}
+	
+	@Test
+	void testStringSize() {
+		assertEquals(5, queueString.size());
+		assertEquals(5, queueString2.size());
+	}
+	
+	@Test
+	void testIntegerIsEmpty() {
+		SimplePriorityQueue<Integer> queueEmpty = new SimplePriorityQueue<Integer>();
+		assertEquals(0, queueEmpty.size());
+		assertEquals("{}", queueEmpty.toString());
+	}
+	
+	
+	@Test
+	void testStringIsEmpty() {
+		SimplePriorityQueue<String> queueEmpty = new SimplePriorityQueue<String>();
+		assertEquals(0, queueEmpty.size());
+		assertEquals("{}", queueEmpty.toString());
+	}
+	
+	@Test
+	void testIntegerIsEmptyAfterClear() {
+		assertEquals(5, queueInt.size());
+		assertEquals("{5, 4, 3, 2, 1}", queueInt.toString());
+		queueInt.clear();
+		assertEquals(0, queueInt.size());
+		assertEquals("{}", queueInt.toString());
+		
+		assertEquals(5, queueInt2.size());
+		assertEquals("{40, 20, 0, -20, -40}", queueInt2.toString());
+		queueInt2.clear();
+		assertEquals(0, queueInt2.size());
+		assertEquals("{}", queueInt2.toString());
+		
+		assertEquals(queueInt2.toString(), queueInt.toString());
+	}
+	
+	@Test
+	void testStringIsEmptyAfterClear() {
+		assertEquals(5, queueString.size());
+		assertEquals("{E, D, C, B, A}", queueString.toString());
+		queueString.clear();
+		assertEquals(0, queueString.size());
+		assertEquals("{}", queueString.toString());
+		
+		assertEquals(5, queueString2.size());
+		assertEquals("{X, S, F, E, A}", queueString2.toString());
+		queueString2.clear();
+		assertEquals(0, queueString2.size());
+		assertEquals("{}", queueString2.toString());
+		
+		assertEquals(queueString2.toString(), queueString.toString());
+	}
+	
+	@Test
+	void testIntegerClear() {
+		queueInt.clear();
+		queueInt2.clear();
+		assertEquals("{}", queueInt.toString());
+		assertEquals("{}", queueInt2.toString());
+		assertEquals(queueInt2.toString(), queueInt.toString());
+	}
+	
+	@Test
+	void testStringClear() {
+		queueString.clear();
+		queueString2.clear();
+		assertEquals("{}", queueString.toString());
+		assertEquals("{}", queueString2.toString());
+		assertEquals(queueString2.toString(), queueString.toString());
+	}
+	
+	@Test
+	void testToStringNoItems() {
+		SimplePriorityQueue<Integer> testQueue = new SimplePriorityQueue<Integer>();
 
-		testQueue.insert("A");
-		testQueue.insert("C");
-		testQueue.insert("B");
-		testQueue.insert("C");
-		testQueue.insert("W");
-		testQueue.insert("D");
-		testQueue.insert("A");
+		assertEquals("{}", testQueue.toString());
+	}
+	
+	@Test
+	void testToStringOneItem() {
+		SimplePriorityQueue<Integer> testQueue = new SimplePriorityQueue<Integer>();
 
-		assertEquals("{W, D, C, C, B, A, A}", testQueue.toString());
+		testQueue.insert(1);
+		
+		assertEquals("{1}", testQueue.toString());
+	}
+	
+	@Test
+	void testToStringTwoItems() {
+		SimplePriorityQueue<Integer> testQueue = new SimplePriorityQueue<Integer>();
+
+		testQueue.insert(1);
+		testQueue.insert(2);
+		
+		assertEquals("{2, 1}", testQueue.toString());
 	}
 
 }
